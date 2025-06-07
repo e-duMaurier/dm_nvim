b9503a78eebd70278d1fa49892a260064f73fddc
e-duMaurier <emmadumaurier@gmail.com>
2025-06-07 17:05:15 +0100

    organised mappings.lua to make it easier to find and edit mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,90 +1,152 @@
--- Hold all the customised keymappings across Neovim, and the various plugins.
--- I want to hold them all here, so it is easier to manage, and verify which commands are used, and where.
+----------------------------------------------------------------------------------------------------
+--                                      GLOBAL SETTINGS & VARIABLES
+----------------------------------------------------------------------------------------------------
 
--- Set leader key
+-- Set leader key (for Normal and Visual modes)
+-- The leader key is used as a prefix for custom keybindings.
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Remap <Space> to <Nop> to allow it to be used as the leader key.
+-- Without this, pressing space might insert a space character.
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
--- Variable for noremap and silent values
+-- Global options for keymap.set (no remap and silent execution)
 local opts = { noremap = true, silent = true }
 
--- delete single character without copying
-vim.keymap.set("n", "x", '"_x', opts)
-
--- Keep last yanked when pasting
-vim.keymap.set("v", "p", '"_dP', opts)
-
--- save file
-vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
-
--- save file without auto-formatting
-vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts)
-
--- Toggle line wrapping
-vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts)
-
--- Buffers
-vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
-vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
-vim.keymap.set("n", "<leader>x", ":Bdelete!<CR>", opts) -- close buffer
-vim.keymap.set("n", "<leader>b", "<cmd>enew<CR>", opts) -- new buffer
-
--- These keymaps are specific to Telescope, they don't need to be in mappings.lua
--- vim.keymap.set('n', '<leader>q', ':Bbye<CR>', opts) -- This is for vim-bbye, can remain here or move to a general buffer management plugin config
-
--- Window management
-vim.keymap.set("n", "<leader>v", "<C-w>v", opts)      -- split window vertically
-vim.keymap.set("n", "<leader>h", "<C-w>s", opts)      -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", opts)     -- make split windows equal width & height
-vim.keymap.set("n", "<leader>xs", ":close<CR>", opts) -- close current split window
-
--- Navigate between splits
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", opts)
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", opts)
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts)
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
-
--- Tabs
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts)   -- open new tab
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", opts) -- close current tab
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>", opts)     --  go to next tab
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts)     --  go to previous tab
-
--- Vertical scroll and centre
-vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
-vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
-
--- Find and centre
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
--- Toggle diagnostics
+-- Variable to track diagnostics state (used for toggling)
 local diagnostics_active = true
 
+----------------------------------------------------------------------------------------------------
+--                                      CORE EDITING & FILE OPERATIONS
+----------------------------------------------------------------------------------------------------
+
+-- Basic Editing Actions
+-- Delete single character without copying it to the clipboard.
+vim.keymap.set("n", "x", '"_x', opts)
+-- Paste without overwriting the last yanked content.
+vim.keymap.set("v", "p", '"_dP', opts)
+
+-- File Saving
+-- Save the current file.
+vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
+-- Save the current file without triggering auto-formatting.
+vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts)
+
+----------------------------------------------------------------------------------------------------
+--                                      BUFFER MANAGEMENT
+----------------------------------------------------------------------------------------------------
+
+-- Navigate between buffers
+-- Go to the next buffer in the buffer list.
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
+-- Go to the previous buffer in the buffer list.
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
+
+-- Buffer Actions
+-- Close the current buffer (similar to :bd or :bdelete).
+vim.keymap.set("n", "<leader>x", ":Bdelete!<CR>", opts)
+-- Create and open a new empty buffer.
+vim.keymap.set("n", "<leader>b", "<cmd>enew<CR>", opts)
+
+----------------------------------------------------------------------------------------------------
+--                                      WINDOW MANAGEMENT
+----------------------------------------------------------------------------------------------------
+
+-- Create New Splits
+-- Split the current window vertically (creates a new window to the right).
+vim.keymap.set("n", "<leader>v", "<C-w>v", opts)
+-- Split the current window horizontally (creates a new window below).
+vim.keymap.set("n", "<leader>h", "<C-w>s", opts)
+
+-- Window Sizing & Closing
+-- Make all split windows in the current tab equal in height and width.
+vim.keymap.set("n", "<leader>se", "<C-w>=", opts)
+-- Close the current window (split).
+vim.keymap.set("n", "<leader>xs", ":close<CR>", opts)
+
+-- Navigate Between Existing Splits (using Ctrl-w + direction)
+-- Move cursor to the window above.
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", opts)
+-- Move cursor to the window below.
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", opts)
+-- Move cursor to the window on the left.
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts)
+-- Move cursor to the window on the right.
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
+
+----------------------------------------------------------------------------------------------------
+--                                      TAB MANAGEMENT
+----------------------------------------------------------------------------------------------------
+
+-- Tab Actions
+-- Open a new empty tab.
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts)
+-- Close the current tab.
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", opts)
+
+-- Navigate Between Tabs
+-- Go to the next tab.
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>", opts)
+-- Go to the previous tab.
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts)
+
+----------------------------------------------------------------------------------------------------
+--                                      NAVIGATION & SCROLLING
+----------------------------------------------------------------------------------------------------
+
+-- Vertical Scrolling & Centering
+-- Scroll down half a screen and center the cursor on the middle line.
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+-- Scroll up half a screen and center the cursor on the middle line.
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+
+-- Search Navigation & Centering
+-- Jump to the next search match and center it on the screen.
+vim.keymap.set("n", "n", "nzzzv")
+-- Jump to the previous search match and center it on the screen.
+vim.keymap.set("n", "N", "Nzzzv")
+
+----------------------------------------------------------------------------------------------------
+--                                      DISPLAY & DIAGNOSTICS (LSP)
+----------------------------------------------------------------------------------------------------
+
+-- Toggle Line Wrapping
+-- Toggle line wrapping on/off for the current buffer.
+vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts)
+
+-- Toggle All Diagnostics
+-- Toggles all LSP diagnostics (errors, warnings, info) on or off for the current buffer.
 vim.keymap.set("n", "<leader>do", function()
-    diagnostics_active = not diagnostics_active
+	diagnostics_active = not diagnostics_active
+	if diagnostics_active then
+		vim.diagnostic.enable(0)
+	else
+		vim.diagnostic.disable(0)
+	end
+end, opts)
 
-    if diagnostics_active then
-        vim.diagnostic.enable(0)
-    else
-        vim.diagnostic.disable(0)
-    end
-end)
-
--- Diagnostic keymaps
+-- Diagnostic Navigation
+-- Go to the previous diagnostic message in the current file.
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+-- Go to the next diagnostic message in the current file.
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+-- Open a floating window with the diagnostic message under the cursor.
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+-- Populate the quickfix list with all diagnostic messages in the current file.
 vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
--- Neotree Toggle (Hide/Show/Open/Close)
-vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", opts)
+----------------------------------------------------------------------------------------------------
+--                                      PLUGIN SPECIFIC MAPPINGS
+----------------------------------------------------------------------------------------------------
 
+-- NeoTree
+-- Toggle the visibility of the NeoTree file explorer sidebar.
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", opts)
+-- Open NeoTree in a floating window to show Git status.
 vim.keymap.set(
-    "n",
-    "<leader>ngs",
-    ":Neotree float git_status<CR>",
-    { silent = true, desc = "Neotree Open Git Status Window" }
+	"n",
+	"<leader>ngs",
+	":Neotree float git_status<CR>",
+	{ silent = true, desc = "Neotree Open Git Status Window" }
 )
--- lua/core/neotree.lua
+++ lua/core/neotree.lua
@@ -1,39 +1,41 @@
 -- Neotree explorer panel
 
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        "3rd/image.nvim",        -- Optional image support in preview window
-    },
-    lazy = false,                -- neo-tree will load itself immediately
-    opts = {
-        window = {
-            position = "left", -- Ensures Neotree always opens on the left
-            width = 30, -- Optional: Set a default width for the Neotree window
-            mappings = {},
-        },
-        filesystem = {
-            follow_current_file = enable,
-            hijack_netrw_behavior = "open_default", -- Prevents default file handling conflicts
-            filtered_items = {
-                visible = true,
-                hide_dotfiles = false,
-                hide_gitignored = true,
-            },
-            window = {
-                mappings = {
-                    -- This mapping uses nvim-window-picker's functionality
-                    ["<CR>"] = "open",
-                    ["o"] = "open",
-                    ["s"] = "open_split", -- Open in a horizontal split
-                    ["v"] = "open_vsplit", -- Open in a vertical split
-                    ["t"] = "open_tabnew", -- Open in a new tab
-                },
-            },
-        },
-    },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		"3rd/image.nvim", -- Optional image support in preview window
+	},
+	lazy = false, -- neo-tree will load itself immediately
+	opts = {
+		window = {
+			position = "left", -- Ensures Neotree always opens on the left
+			width = 30, -- Optional: Set a default width for the Neotree window
+			mappings = {},
+		},
+		filesystem = {
+			follow_current_file = {
+				enabled = true,
+			},
+			hijack_netrw_behavior = "open_default", -- Prevents default file handling conflicts
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = true,
+			},
+			window = {
+				mappings = {
+					-- This mapping uses nvim-window-picker's functionality
+					["<CR>"] = "open",
+					["o"] = "open",
+					["s"] = "open_split", -- Open in a horizontal split
+					["v"] = "open_vsplit", -- Open in a vertical split
+					["t"] = "open_tabnew", -- Open in a new tab
+				},
+			},
+		},
+	},
 }

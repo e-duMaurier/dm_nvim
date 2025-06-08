2db6254573818bf7ed3e98f57f43b978696ca66d
e-duMaurier <emmadumaurier@gmail.com>
2025-06-08 20:57:56 +0100

    added comment to autocmd.lua

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,44 +1,45 @@
--- Some usefull scripts/functions to enhance functionality/workflow while in Nvim.
+-- lua/config/autocmd.lua
+-- Contains various useful autocommands for Neovim functionality and workflow.
 
 -- close nvim-tree if it's last buffer open
 vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = "*",
-    callback = function()
-        if #vim.api.nvim_list_bufs() == 1 and vim.bo.filetype == "Neotree" then
-            vim.cmd("quit")
-        end
-    end,
+	pattern = "*",
+	callback = function()
+		if #vim.api.nvim_list_bufs() == 1 and vim.bo.filetype == "Neotree" then
+			vim.cmd("quit")
+		end
+	end,
 })
 
 -- spellcheck in md
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "markdown",
-    command = "setlocal spell wrap",
+	pattern = "markdown",
+	command = "setlocal spell wrap",
 })
 
 -- disable automatic comment on newline
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*",
-    callback = function()
-        vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-    end,
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
 })
 
 -- highlight text on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank({ timeout = 300 })
-    end,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ timeout = 300 })
+	end,
 })
 
 -- restore cursor pos on file open
 vim.api.nvim_create_autocmd("BufReadPost", {
-    pattern = "*",
-    callback = function()
-        local line = vim.fn.line("'\"")
-        if line > 1 and line <= vim.fn.line("$") then
-            vim.cmd("normal! g'\"")
-        end
-    end,
+	pattern = "*",
+	callback = function()
+		local line = vim.fn.line("'\"")
+		if line > 1 and line <= vim.fn.line("$") then
+			vim.cmd("normal! g'\"")
+		end
+	end,
 })
